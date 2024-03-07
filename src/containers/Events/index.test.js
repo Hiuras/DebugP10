429da3ff749ef7bbc2ad9ab7a48ffc1e4cc321b1
Hiuras <Corentin.lejeune@orange.fr>
2024-03-07 10:42:13 +0100

    Correction Event

--- src/containers/Events/index.test.js
+++ src/containers/Events/index.test.js
@@ -2,6 +2,7 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { api, DataProvider } from "../../contexts/DataContext";
 import Events from "./index";
 
+// Les données simulées pour les tests
 const data = {
   events: [
     {
@@ -9,7 +10,7 @@ const data = {
       type: "soirée entreprise",
       date: "2022-04-29T20:28:45.744Z",
       title: "Conférence #productCON",
-      cover: "/images/event/stem-list-EVgsAbL51Rk-unsplash.png",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
       description:
         "Présentation des outils analytics aux professionnels du secteur",
       nb_guesses: 1300,
@@ -21,13 +22,12 @@ const data = {
         "1 site web dédié",
       ],
     },
-
     {
       id: 2,
       type: "forum",
       date: "2022-04-29T20:28:45.744Z",
       title: "Forum #productCON",
-      cover: "/images/event/stem-list-EVgsAbL51Rk-unsplash.png",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
       description:
         "Présentation des outils analytics aux professionnels du secteur",
       nb_guesses: 1300,
@@ -37,57 +37,89 @@ const data = {
   ],
 };
 
+// Suite de tests pour le composant Events
 describe("When Events is created", () => {
+  // Test : une liste de cartes d'événements est affichée
   it("a list of event card is displayed", async () => {
+    // Simulation de l'appel à une fonction API pour charger les données
     api.loadData = jest.fn().mockReturnValue(data);
+    // Rendu du composant Events dans le DataProvider
     render(
       <DataProvider>
         <Events />
       </DataProvider>
     );
+    // Attente de l'affichage du texte contenant "avril" (mois de l'événement)
     await screen.findByText("avril");
   });
+
+  // Suite de tests pour le cas où une erreur survient
   describe("and an error occured", () => {
+    // Test : un message d'erreur est affiché
     it("an error message is displayed", async () => {
+      // Simulation de l'appel à une fonction API qui échoue
       api.loadData = jest.fn().mockRejectedValue();
+      // Rendu du composant Events dans le DataProvider
       render(
         <DataProvider>
           <Events />
         </DataProvider>
       );
+      // Vérification que le texte "An error occured" est affiché
       expect(await screen.findByText("An error occured")).toBeInTheDocument();
     });
   });
+
+  // Suite de tests pour le cas où une catégorie est sélectionnée
   describe("and we select a category", () => {
-    it("a filtered list is displayed", async () => {
+    // Test : une liste filtrée est affichée
+    it.only("an filtered list is displayed", async () => {
+      // Simulation de l'appel à une fonction API pour charger les données
       api.loadData = jest.fn().mockReturnValue(data);
+      // Rendu du composant Events dans le DataProvider
       render(
         <DataProvider>
           <Events />
         </DataProvider>
       );
+      // Attente de l'affichage du texte d'un événement
       await screen.findByText("Forum #productCON");
-  
-      const collapseButton = screen.getByTestId("collapse-button-testid");
-      fireEvent.click(collapseButton);
-  
-      const categoryButton = await screen.findByText("soirée entreprise");
-      fireEvent.click(categoryButton);
-  
+      // Déclenchement d'un clic sur le bouton de collapse (masquage)
+      fireEvent(
+        await screen.findByTestId("collapse-button-testid"),
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      // Déclenchement d'un clic sur un élément du filtre de catégorie
+      fireEvent(
+        (await screen.findAllByText("soirée entreprise"))[0],
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      // Attente de l'affichage du texte d'un événement de la catégorie sélectionnée
       await screen.findByText("Conférence #productCON");
+      // Vérification que l'événement non filtré n'est plus présent
       expect(screen.queryByText("Forum #productCON")).not.toBeInTheDocument();
     });
   });
 
+  // Suite de tests pour le cas où un événement est cliqué
   describe("and we click on an event", () => {
+    // Test : les détails de l'événement sont affichés
     it("the event detail is displayed", async () => {
+      // Simulation de l'appel à une fonction API pour charger les données
       api.loadData = jest.fn().mockReturnValue(data);
+      // Rendu du composant Events dans le DataProvider
       render(
         <DataProvider>
           <Events />
         </DataProvider>
       );
-
+      // Déclenchement d'un clic sur le texte de l'événement
       fireEvent(
         await screen.findByText("Conférence #productCON"),
         new MouseEvent("click", {
@@ -95,8 +127,9 @@ describe("When Events is created", () => {
           bubbles: true,
         })
       );
-
+      // Attente de l'affichage du texte de la période de l'événement
       await screen.findByText("24-25-26 Février");
+      // Attente de l'affichage du texte d'une prestation de l'événement
       await screen.findByText("1 site web dédié");
     });
   });
