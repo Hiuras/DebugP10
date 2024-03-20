86582dfecf834eaab1db925dd4a38cddc7832626
Hiuras <137842225+Hiuras@users.noreply.github.com>
2024-03-20 12:54:32 +0100

    Fix Home

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -10,37 +10,29 @@ import "./style.css";
 const PER_PAGE = 9;
 
 const EventList = () => {
-  const { data, error } = useData(); // Récupération des données et des erreurs éventuelles
-  const [type, setType] = useState(); // État pour la catégorie sélectionnée
-  const [currentPage, setCurrentPage] = useState(1); // État pour la page actuelle
-
-  let filteredEvents = []; // Initialisation du tableau d'événements filtrés
-  // Filtre en fonction du type sélectionné
-  if (data?.events) {
-    if (type) {
-      filteredEvents = data.events.filter((event) => event.type === type);
-    } else {
-      filteredEvents = data.events;
+  const { data, error } = useData();
+  const [type, setType] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+  const filteredEvents = (
+    (!type
+      // Affiche tous les événements
+      ? data?.events
+      // Permet de filtrer les événements en fonction de leur type
+      : data?.events.filter((event) => event.type === type)) || []
+  ).filter((event, index) => {
+    if (
+      (currentPage - 1) * PER_PAGE <= index &&
+      PER_PAGE * currentPage > index
+    ) {
+      return true;
     }
-  }
-  
-  // Trier les événements filtrés par date du plus récent au plus ancien
-  filteredEvents.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  // Pagination des événements filtrés
-  filteredEvents = filteredEvents.filter(
-    (event, index) =>
-      (currentPage - 1) * PER_PAGE <= index && PER_PAGE * currentPage > index
-  );
-
-  // Fonction pour changer le type de catégorie et réinitialiser la page
+    return false;
+  });
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  // Création d'un ensemble de types pour le menu déroulant
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
@@ -49,34 +41,26 @@ const EventList = () => {
         "loading"
       ) : (
         <>
-          {/* Affichage du titre de la sélection */}
           <h3 className="SelectTitle">Catégories</h3>
-          {/* Sélection du type d'événement à afficher */}
           <Select
             selection={Array.from(typeList)}
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
-          {/* Conteneur pour afficher les événements filtrés */}
-          <div id="events" className="ListContainer">
-            {/* Affichage de chaque événement filtré sous forme de carte événementielle */}
-            {filteredEvents.map(
-              (event) =>
-                event && (
-                  <Modal key={event.id} Content={<ModalEvent event={event} />}>
-                    {({ setIsOpened }) => (
-                      <EventCard
-                        onClick={() => setIsOpened(true)}
-                        imageSrc={event.cover}
-                        title={event.title}
-                        date={new Date(event.date)}
-                        label={event.type}
-                      />
-                    )}
-                  </Modal>
-                )
-            )}
+          <div id="events" className="ListContainer" data-testid="listEvents">
+            {filteredEvents.map((event) => (
+              <Modal key={event.id} Content={<ModalEvent event={event} />}>
+                {({ setIsOpened }) => (
+                  <EventCard
+                    onClick={() => setIsOpened(true)}
+                    imageSrc={event.cover}
+                    title={event.title}
+                    date={new Date(event.date)}
+                    label={event.type}
+                  />
+                )}
+              </Modal>
+            ))}
           </div>
-          {/* Pagination pour naviguer entre les pages d'événements */}
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,43 +1,48 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
 const Slider = () => {
-  // Obtenez les données à partir du contexte DataContext
   const { data } = useData();
-
-  // État pour suivre l'index de la slide actuel
   const [index, setIndex] = useState(0);
+  const [isAutoChange, setIsAutoChange] = useState(true);
 
-  // Triez les données par date dans l'ordre décroissant
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-
-  // Effet pour changer automatiquement la diapositive toutes les 5 secondes
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex(prevIndex => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
+      if (isAutoChange) {
+        setIndex(prevIndex =>
+          prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
+        );
+      }
     }, 5000);
-    
-    // Remet à zéro l'intervalle au changement d'image, qu'il soit automatique ou manuel
-    return () => clearInterval(interval);
-  }, [byDateDesc]);
 
-  // Fonction pour changer de slide lorsqu'un bouton radio est cliqué
-  const handleRadioChange = (radioIdx) => {
+    return () => clearInterval(interval);
+  }, [isAutoChange]);
+
+  const byDateDesc = data?.focus ? [...data.focus].sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  ) : [];
+
+  const handleRadioChange = radioIdx => {
     setIndex(radioIdx);
+    setIsAutoChange(false);
   };
+
+  useEffect(() => {
+    setIsAutoChange(true);
+  }, [index]);
 
   return (
     <div className="SlideCardList">
-      {/* Mappez les données et affichez chaque diapositive */}
-      {byDateDesc?.map((event, idx) => (
+      {byDateDesc.map((event, idx) => (
         <div key={event.id}>
-          {/* Affiche la diapositive seulement si son index correspond à l'index actuel */}
-          <div className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+          <div
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -49,11 +54,9 @@ const Slider = () => {
           </div>
         </div>
       ))}
-      {/* Affiche les boutons de pagination */}
       <div className="SlideCard__paginationContainer">
         <div className="SlideCard__pagination">
-          {byDateDesc?.map((event, radioIdx) => (
-            // Bouton radio qui est coché si son index correspond à l'index actuel
+          {byDateDesc.map((event, radioIdx) => (
             <input
               key={event.id}
               type="radio"
